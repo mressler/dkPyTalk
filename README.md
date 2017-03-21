c6c55da52c92c446d2e0c4601e311e9b4efd5a26
Michael Ressler <mressler@gmail.com>
2017-03-21 00:23:45 -0400

    First pass at models.

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ The Internet of Swings for Code &amp; Supply on March 22nd
 ## You are presently on Step 2
 In Step 2, we will:
 1. Start adding some model objects
-
-Some commentary here - Django makes a big deal about the difference between apps & projects that I'm just glazing over completely here. For my needs, I haven't had to care a lot about the difference. So I just hack this project into also being an app. <3
+   * Users and Swings are a good start
+   * Be sure to run `python manage.py makemigrations polls`
+   * Diamond Kinetics' installation does not rely on migrations
+   * [This Django reference is great for creating Model objects](https://docs.djangoproject.com/en/1.10/ref/models/)
+2. Add in test data
 
 ## Compare a previous step to this step
 * [Step 1](https://github.com/mressler/dkPyTalk/compare/step-1...step-2)
---
+++ internetOfSwings/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class User(models.Model):
+    # Hugely important to Django that you include a field marked a primary_key=True
+    userId = models.IntegerField(primary_key=True)
+    email = models.CharField(max_length=50)
+
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    profileImageUrl = models.CharField(max_length=120)
+
+    class Meta:
+        db_table = 'users'
+        default_related_name = db_table
+        app_label = 'internetOfSwings'
+
+    def __str__(self):
+        return "%s %s (%s)" % (self.firstName, self.lastName, self.email)
+
+
+class Swing(models.Model):
+    swing = models.IntegerField(primary_key=True)
+
+    # Relate back to the owning User
+    user = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        db_column='user'
+    )
+
+    data = models.TextField()
+
+    # Representative metric placeholder
+    batSpeed = models.FloatField()
+
+    class Meta:
+        db_table = 'swings'
+        default_related_name = db_table
+        app_label = 'internetOfSwings'
